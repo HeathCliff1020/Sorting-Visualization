d18794c60481fb1a85fe10add2aa002ef02f3f81
HeathCliff <HeathCliff1020@gmail.com>
2019-11-07 18:43:18 +0530

    Way to number select

--- script/style.js
+++ script/style.js
@@ -1,5 +1,14 @@
 $(document).ready(function()
 {
+
+	for(var i=1; i<=100; i++){
+    	var select = document.getElementById("numbers");
+	    var option = document.createElement("OPTION");
+	    select.options.add(option);
+	    option.text = i;
+	    option.value = i;
+	}
+
 	$('.menu_open').click(function()
 	{
 		$('.vert_nav').toggleClass('vert_nav_open');
